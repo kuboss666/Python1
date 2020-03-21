8a741dc80fa8a1b646cd832ba52465431c0cee15
kuboss666 <62471085+kuboss666@users.noreply.github.com>
2020-03-21 18:07:21 +0100

    kjlj

--- Start_20200321.py
+++ Start_20200321.py
@@ -1,12 +1,19 @@
 def first_1():
-    numb1=1
-    numb2=2
+    numb1 = 1
+    numb2 = 2
 
-    if (numb1==numb2):
+    if numb1 == numb2:
         print ("Hurra")
     else:
         print ("nicht huure")
 
 
+    f = open("guru99.txt", "w+")
+    f.write("first line")
+    f.close()
+
+
+
 def Print_test():
     print("Printing")
+    print("22")
